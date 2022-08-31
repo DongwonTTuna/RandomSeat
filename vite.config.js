c8086d4e1af71354f4a3034abf904e88b786c2f0
dongwon <ttuna0790@gmail.com>
2022-08-31 14:19:12 +0900

    y

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     assetsInlineLimit: 2048000,
     rollupOptions: {
       output: {
-        entryFileNames: "static/js/randomseat.js",
-        chunkFileNames: "static/js/randomseat.chunk.js",
+        entryFileNames: "static/randomseat/js/randomseat.js",
+        chunkFileNames: "static/randomseat/js/randomseat.chunk.js",
         assetFileNames: (assetsInfo) => {
           let extType = assetsInfo.name.split(".").pop();
           if (/png|jpe?g|svg|gif|tiff|bmp|ico|svg/i.test(extType)) {
